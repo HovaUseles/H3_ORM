7761134bbbaea974032211e5753e9fff4412413c
HovaUseless <88477974+HovaUseles@users.noreply.github.com>
2023-08-14 12:17:00 +0200

    Removed Unused projects

--- H3_ORM/H3_ORM_GUI/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using H3_ORM_GUI.GUI.Controllers;
-using H3_ORM_GUI.GUI.Utilities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-// Displaying first View
-ViewRenderer.RenderView(HomeController.Index());
